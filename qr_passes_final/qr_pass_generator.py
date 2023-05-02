aebd6a11ff5b4d7269bf4217de4a22b13e16059c
MrKing-dev <grekin@hasdhawks.org>
2023-05-01 20:45:11 -0400

    M qr_passes_final/qr_pass_generator.py

--- qr_passes_final/qr_pass_generator.py
+++ qr_passes_final/qr_pass_generator.py
@@ -193,7 +193,7 @@ for i in range(2):
         index = i*4 + j
         
         class_name = classes[index]
-        b = ttkbs.Button(period_frame, text=class_name, width=15, bootstyle="success-outline", command=lambda x=class_name: update_class(x))
+        b = ttkbs.Button(period_frame, text=class_name, width=15, bootstyle="success-outline", command=lambda x=class_name, btn=b: update_class(x, btn))
         b.grid(row=i, column=j, padx=10, pady=10)
             
 #create 4x5 grid of buttons with student numbers
